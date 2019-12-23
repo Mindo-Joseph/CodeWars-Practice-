0d598b40a93b7fdfe43e4d581001695b1845c52b
Mindo-Joseph <mindo.joseph.mj@gmail.com>
2019-12-23 21:06:28 +0300

    find out whether an integer is equal to the sum of its digits raised to the number of digits in the integer

---
+++ does_my_number_look_big_in_this.py
@@ -0,0 +1,13 @@
+def narcissistic ( value ):
+    total = 0
+    value = str(value)
+    length = len(value)
+    for i in value:
+        total += pow(int(i),length)
+    return total == int(value)
+   
+
+print(narcissistic(153))
+        
+
+    
